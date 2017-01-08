0a35c7b9ab1f328585b713a12dc55f67225742b8
Thomas Maier <contact@thomas-maier.net>
2017-01-08 23:03:45 +0100

    delete archive after setup

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 wget -v https://github.com/opthomas-prime/braindump/archive/master.zip -O /tmp/braindump-master.zip
 unzip -v /tmp/braindump-master.zip -d /tmp
 sudo install -Tv -m 777 /tmp/braindump-master/braindump.py /usr/local/bin/braindump.py
+rm -v /tmp/braindump-master.zip
+rm -vr /tmp/braindump-master
 sudo ln -vs /usr/local/bin/braindump.py /usr/local/bin/brian
 sudo pip3 install whoosh
