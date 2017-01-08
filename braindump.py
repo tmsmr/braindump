6a7c5ab00e90528acb7ddce48503df31494d93b4
Thomas Maier <contact@thomas-maier.net>
2017-01-08 15:50:52 +0100

    working prototype

---
+++ braindump.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import shlex
+import subprocess as sp
+
+
+class Brain:
+    def __init__(self, config):
+        from whoosh.fields import TEXT, ID, Schema
+        from whoosh.index import create_in, open_dir
+        self.editor = config.get('brain', 'editor')
+        self.storage_path = os.path.expanduser(config.get('brain', 'storage'))
+        self.index_path = os.path.expanduser(config.get('brain', 'index'))
+        if not os.path.exists(self.storage_path):
+            os.makedirs(self.storage_path)
+        if not os.path.exists(self.index_path):
+            os.makedirs(self.index_path)
+            schema = Schema(entry=ID(stored=True, unique=True), content=TEXT(stored=True))
+            self.index = create_in(self.index_path, schema)
+            self.index_all()
+        else:
+            self.index = open_dir(self.index_path)
+
+    @staticmethod
+    def entry_name():
+        import random
+        import string
+        from datetime import datetime
+        date_str = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
+        random_str = ''.join(random.choice(string.hexdigits) for _ in range(8)).upper()
+        return date_str + '_' + random_str
+
+    def index_entry(self, entry_path, entry_name):
+        if not os.path.isfile(entry_path):
+            return False
+        writer = self.index.writer()
+        with open(entry_path) as entry_file:
+            entry_content = ''.join(entry_file.readlines())
+        writer.update_document(entry=entry_name, content=entry_content)
+        writer.commit()
+        return True
+
+    def index_all(self):
+        for entry in os.listdir(self.storage_path):
+            entry_path = os.path.join(self.storage_path, entry)
+            entry_name = os.path.splitext(entry)[0]
+            if os.path.isfile(entry_path) and os.path.splitext(entry)[-1] == '.entry':
+                self.index_entry(entry_path, entry_name)
+
+    def dump_entry(self):
+        return self.edit_entry(self.entry_name())
+
+    def edit_entry(self, entry_name):
+        entry_path = os.path.join(self.storage_path, entry_name + '.entry')
+        cmd = shlex.split(self.editor + ' ' + entry_path)
+        if sp.call(cmd):
+            return False
+        return self.index_entry(entry_path, entry_name)
+
+    @staticmethod
+    def choose_result(results):
+        from whoosh import highlight
+
+        class TerminalFormatter(highlight.Formatter):
+            def __init__(self):
+                pass
+
+            def format_token(self, text, token, replace=False):
+                no_color = '\033[0m'
+                hi_color = '\033[0;33m'
+                return hi_color + highlight.get_text(text, token, replace) + no_color
+
+        results.formatter = TerminalFormatter()
+        while True:
+            for i, result in enumerate(results):
+                print(str(i + 1) + '. ' + result.highlights('content'))
+            try:
+                num = input('number to open: ')
+                res_index = int(num) - 1
+                if 0 <= res_index < len(results):
+                    return results[res_index]
+            except ValueError:
+                continue
+            except KeyboardInterrupt:
+                return None
+
+    def remember(self, tokens, limit=10):
+        from whoosh.query import And, Term
+        terms = []
+        for token in tokens:
+            terms.append(Term('content', token))
+        query = And(terms)
+        with self.index.searcher() as searcher:
+            results = searcher.search(query, limit=limit)
+            if len(results) == 1:
+                self.edit_entry(results[0]['entry'])
+            elif len(results) > 1:
+                result = self.choose_result(results)
+                if result:
+                    self.edit_entry(result['entry'])
+            else:
+                return False
+        return True
+
+
+def get_args():
+    from argparse import ArgumentParser
+    parser = ArgumentParser('braindump')
+    parser.add_argument('operation', help='the operation: dump (d), remember (r)')
+    parser.add_argument('terms', help='search terms (for remember operation)', nargs='*')
+    parser.add_argument('--config', default='~/.brain.conf', help='config file (defaults to ~/.brain.conf)')
+    return parser.parse_args()
+
+
+def guess_editor(config_path):
+    editors = ['vim', 'emacs', 'nano']
+    for editor in editors:
+        cmd = shlex.split('hash ' + editor)
+        if not sp.call(cmd, shell=True):
+            return editor
+    print('failed to find an editor, please set value manually in ' + config_path)
+    return ''
+
+
+def get_conf(config_path):
+    from configparser import ConfigParser
+    expanded_path = os.path.expanduser(config_path)
+    if not os.path.isfile(expanded_path):
+        config = ConfigParser()
+        config.add_section('brain')
+        config.set('brain', 'storage', '~/.brain')
+        config.set('brain', 'index', '~/.brain/index')
+        config.set('brain', 'editor', guess_editor(config_path))
+        with open(expanded_path, 'w') as configfile:
+            config.write(configfile)
+        return config
+    config = ConfigParser()
+    config.read(expanded_path)
+    return config
+
+
+def main():
+    args = get_args()
+    config = get_conf(args.config)
+    brain = Brain(config)
+    if args.operation == 'dump' or args.operation == 'd':
+        if not brain.dump_entry():
+            print('unable to dump entry')
+    elif args.operation == 'remember' or args.operation == 'r':
+        if len(args.terms) < 1:
+            print('no terms given')
+        else:
+            if not brain.remember(args.terms):
+                print('no entries found')
+
+
+if __name__ == "__main__":
+    main()
