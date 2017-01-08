9c7c4dd161a8f4fa512e627962b9d9558e9e676e
Thomas Maier <contact@thomas-maier.net>
2017-01-08 23:11:57 +0100

    changed setup routine

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-wget -v https://github.com/opthomas-prime/braindump/archive/master.zip -O /tmp/braindump-master.zip
-unzip -v /tmp/braindump-master.zip -d /tmp
-sudo install -Tv -m 777 /tmp/braindump-master/braindump.py /usr/local/bin/braindump.py
-rm -v /tmp/braindump-master.zip
-rm -vr /tmp/braindump-master
+STARTDIR=`pwd`
+mkdir /tmp/braindump && cd /tmp/braindump
+wget https://github.com/opthomas-prime/braindump/archive/master.zip
+unzip master.zip
+sudo install -Tv -m 777 braindump-master/braindump.py /usr/local/bin/braindump.py
 sudo ln -vs /usr/local/bin/braindump.py /usr/local/bin/brian
 sudo pip3 install whoosh
+cd $STARTDIR
