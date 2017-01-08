2447a36b804f9ffb70ca806d47f9cfefd05c81fc
Thomas Maier <contact@thomas-maier.net>
2017-01-08 19:10:41 +0100

    i like that name actually

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 wget -v https://github.com/opthomas-prime/braindump/archive/master.zip -O /tmp/braindump-master.zip
 unzip -v /tmp/braindump-master.zip -d /tmp
 sudo install -Tv -m 777 /tmp/braindump-master/braindump.py /usr/local/bin/braindump.py
-sudo ln -vs /usr/local/bin/braindump.py /usr/local/bin/brain
+sudo ln -vs /usr/local/bin/braindump.py /usr/local/bin/brian
 sudo pip3 install whoosh
